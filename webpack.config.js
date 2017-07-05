ca063ded894e8c38c15a7bd630b93a72e4d0c1d6
Worldofwbdesign <Worldofwbdesign@users.noreply.github.com>
2017-07-05 10:18:07 +0300

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,40 +5,40 @@ var envFile = require('node-env-file');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 try {
-  envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV + '.env'));
+    envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV + '.env'));
 } catch (e) {
 
 }
 
 module.exports = {
-  entry: [
+    entry: [
     './src/index.js'
   ],
-  output: {
-    path: __dirname,
-    filename: './public/bundle.js'
-  },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['react', 'es2015', 'stage-1']
+            }
     }]
-  },
-  plugins: [
+    },
+    plugins: [
     new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    }),
+            compressor: {
+                warnings: false
+            }
+        }),
   ],
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './'
+    }
 };
