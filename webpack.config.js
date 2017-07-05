1b78e8e1013942b6b97ed76e0b53b9e6aaea4c06
Worldofwbdesign <worldofwbdesign@gmail.com>
2017-07-05 07:56:52 +0300

    Fix deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
+var webpack = require('webpack');
+var path = require('path');
+var envFile = require('node-env-file');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+try {
+  envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV + '.env'));
+} catch (e) {
+
+}
+
 module.exports = {
   entry: [
     './src/index.js'
   ],
   output: {
     path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    filename: './public/bundle.js'
   },
   module: {
     loaders: [{
@@ -16,6 +27,13 @@ module.exports = {
       }
     }]
   },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    }),
+  ]
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
