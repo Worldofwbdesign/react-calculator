27783a0c67d9fa0e4d13c14c758f9abc8b7e2156
Worldofwbdesign <worldofwbdesign@gmail.com>
2017-07-24 20:14:57 +0300

    fix deploy config for heroku

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 
 // Create our app
 var app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'https') {
@@ -14,6 +13,6 @@ app.use(function (req, res, next){
 
 app.use(express.static('public'));
 
-app.listen(PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Express server is up on port ' + PORT);
 });
